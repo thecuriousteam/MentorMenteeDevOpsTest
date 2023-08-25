993ad39c66a4c60fbadb587ccd672d24480317db
Nandan G N <curioustechnologies.org@gmail.com>
2023-08-25 18:06:36 +0530

    test

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Data = ({apiData}) => {
   return (
     <div>
+      <h1>Love You Kajal</h1>
         <ul>
             {apiData?.map(data => (
                 <li key = {data.id}>{data.title}</li>
